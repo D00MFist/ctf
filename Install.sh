a23e9fa7e861b4a811cce8397d69cbbbb0cb3211
Pittst0p <Pittst0p@yahoo.com>
2017-10-18 15:15:42 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "So this doesn't do anything besides copy everything to /opt/ctf; you lazy turd"
+cd /opt
+git clone https://github.com/Pittst0p/ctf.git
+echo "Script installed in /opt/ctf"
